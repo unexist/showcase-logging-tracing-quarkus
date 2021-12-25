0d94f20804b39868de56220178755af1137b65a4
Christoph Kappel <christoph@unexist.dev>
2021-12-25 17:49:22 +0100

    Fixed updated packages in Smallrye

--- todo-service-check/src/test/java/dev/unexist/showcase/todo/application/TodoSourceTest.java
+++ todo-service-check/src/test/java/dev/unexist/showcase/todo/application/TodoSourceTest.java
@@ -1,8 +1,19 @@
+/**
+ * @package Quarkus-Logging-Tracing-Quarkus
+ *
+ * @file Stupid integration test
+ * @copyright 2020-2021 Christoph Kappel <christoph@unexist.dev>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the Apache License v2.0.
+ * See the file LICENSE for details.
+ **/
+
 package dev.unexist.showcase.todo.application;
 
 import io.quarkus.test.junit.QuarkusTest;
-import io.smallrye.reactive.messaging.connectors.InMemoryConnector;
-import io.smallrye.reactive.messaging.connectors.InMemorySink;
+import io.smallrye.reactive.messaging.providers.connectors.InMemoryConnector;
+import io.smallrye.reactive.messaging.providers.connectors.InMemorySink;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
