5767a401f88f8dd6ac6e2d0cf88e22d8434cafb9
Christoph Kappel <christoph@unexist.dev>
2022-01-06 17:02:12 +0100

    Added kafka Todo deserializer

---
+++ todo-service-verify/src/main/java/dev/unexist/showcase/todo/infrastructure/serde/TodoDeserializer.java
@@ -0,0 +1,26 @@
+/**
+ * @package Quarkus-Logging-Tracing-Quarkus
+ *
+ * @file Todo deserializer
+ * @copyright 2022 Christoph Kappel <christoph@unexist.dev>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the Apache License v2.0.
+ * See the file LICENSE for details.
+ **/
+
+package dev.unexist.showcase.todo.infrastructure.serde;
+
+import dev.unexist.showcase.todo.domain.todo.Todo;
+import io.quarkus.kafka.client.serialization.ObjectMapperDeserializer;
+
+public class TodoDeserializer extends ObjectMapperDeserializer<Todo> {
+
+    /**
+     * Construct deserializer for {@link Todo}
+     **/
+
+    public TodoDeserializer() {
+        super(Todo.class);
+    }
+}
